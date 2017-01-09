cde45d2ef5cb122450dd15d9ead81815d109e699
whoismy8023 <whoismy8023@163.com>
2017-01-09 17:53:04 +0800

    修改freemaker配置

--- src/main/java/net/sppan/base/config/FreeMarkerConfig.java
+++ src/main/java/net/sppan/base/config/FreeMarkerConfig.java
@@ -16,8 +16,6 @@ public class FreeMarkerConfig {
     @PostConstruct
     public void setSharedVariable() {
     	try {
-    		configuration.setDateFormat("yyyy/MM/dd");  
-            configuration.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
 			configuration.setSharedVariable("testSharedVariable", "111111111111111111111");
 		} catch (TemplateModelException e) {
 			e.printStackTrace();
