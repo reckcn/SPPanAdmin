50322e3fe85429b7d76ef6a1f2e047194e5180ba
whoismy8023@163.com <whoismy8023@163.com>
2017-10-13 11:02:15 +0800

    加入tomcat部署支持

--- src/main/java/net/sppan/base/Application.java
+++ src/main/java/net/sppan/base/Application.java
@@ -4,16 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application{
+public class Application extends SpringBootServletInitializer {
 
-	private static Logger logger = LoggerFactory.getLogger(Application.class);
-	/**
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		logger.debug("启动成功");
-	}
-	
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    /**
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        logger.debug("启动成功");
+    }
+
 }
